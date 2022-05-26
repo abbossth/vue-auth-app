0fc449c720038784073a27c460bd03c57e76ad24
abbossth <bosskodevelopment@gmail.com>
2022-05-26 19:31:41 +0500

    Template is ready

---
+++ src/components/Nav.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand">AUTH-APP</a>
+      <div class="d-flex">
+        <button class="btn btn-outline-success me-2" type="button">Log In</button>
+        <button class="btn btn-sm btn-outline-secondary" type="button">Register</button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Navbar'
+}
+</script>
+
+<style>
+
+</style>
